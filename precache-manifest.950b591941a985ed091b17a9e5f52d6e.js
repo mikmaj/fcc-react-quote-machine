bd86ae9ce06c7f897f4993c0f544a0be47f4cb4c
Mikko Maja <mikmaj88@gmail.com>
2019-01-13 19:49:38 +0200

    Updates

--- precache-manifest.950b591941a985ed091b17a9e5f52d6e.js
+++ precache-manifest.950b591941a985ed091b17a9e5f52d6e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fcc-react-quote-machine/static/js/runtime~main.f7911777.js"
   },
   {
-    "revision": "abec9a7ae04b78771ddd",
-    "url": "/fcc-react-quote-machine/static/js/main.abec9a7a.chunk.js"
+    "revision": "738eeab60f1af83483f7",
+    "url": "/fcc-react-quote-machine/static/js/main.738eeab6.chunk.js"
   },
   {
     "revision": "f739feb7d61c48a46bed",
     "url": "/fcc-react-quote-machine/static/js/1.f739feb7.chunk.js"
   },
   {
-    "revision": "abec9a7ae04b78771ddd",
-    "url": "/fcc-react-quote-machine/static/css/main.b693c67a.chunk.css"
+    "revision": "738eeab60f1af83483f7",
+    "url": "/fcc-react-quote-machine/static/css/main.4912f61b.chunk.css"
   },
   {
-    "revision": "40aea4f175aa222fa0843db3dbcc1afc",
+    "revision": "f2b9838b80b1000daf92c260199df154",
     "url": "/fcc-react-quote-machine/index.html"
   }
 ];
